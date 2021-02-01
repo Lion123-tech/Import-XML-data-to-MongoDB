15daa24b646e9b763fae78ee906fd62638c281a9
Shivangi <cu.18bcs2250@gmail.com>
2021-02-01 15:58:13 +0530

    deploy2

--- datamodel.js
+++ datamodel.js
@@ -1,11 +1,7 @@
 const mongoose=require('mongoose');
 const dataschema=new mongoose.Schema({
     Name:String,
-    Roll_no:{
-        type:String,
-        unique:[true,"Name not"]
-    },
-   
+    Roll_no:String,
     Class:Number
 });
 module.exports=mongoose.model("datastore23",dataschema);
