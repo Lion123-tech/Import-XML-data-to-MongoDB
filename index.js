3b1291a9f68970ee37d9f2613199d14ff9097d6e
Shivangi <cu.18bcs2250@gmail.com>
2021-02-01 16:31:56 +0530

    deploy3

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const path=require('path');
 const fileupload = require("express-fileupload");
 const routf=require('./routf');
 const app=new express();
-const DB="mongodb+srv://Shivangi:Zigzag1097@cluster0.y1ibt.mongodb.net/firstdb?retryWrites=true&w=majority";
+const PORT=process.env.PORT || 9000 ;
+const DB=process.env.MONGO_URL || "mongodb+srv://Shivangi:Zigzag1097@cluster0.y1ibt.mongodb.net/firstdb?retryWrites=true&w=majority";
 mongoose.connect(DB,{
     useNewUrlParser:true,
     useCreateIndex:true,
@@ -24,7 +25,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set("view engine","hbs");
 app.use(express.static(__dirname + '/public'));
 //
-app.listen(process.env.port1 || 7888,()=>{
+app.listen(PORT,()=>{
     console.log("Waiting for requset!");
 });
 app.route("/").get(routf.sendpage);
